5e17c99886df7ef7aa5e43258416b37e5168edb2
ilovenoah <ilovenoah0105@gmail.com>
2024-03-25 19:14:29 +0900

    tmp commit

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -6,9 +6,41 @@ if [ -z "$MYSQL_ROOT_PASSWORD" -o -z "$MYSQL_DATABASE" -o -z "$MYSQL_USER" -o -z
   exit 1
 fi
 
-#initialize database directory
+# initialize database directory
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   echo "Initializing MariaDB data directory..."
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
+# start MariaDB server
+echo "Starting MariaDB server..."
+mysqld --user=mysql &
+
+# wait for MariaDB server to start
+until mysqladmin ping -h"localhost" --silent; do
+  echo "Waiting for MariaDB server to start..."
+  sleep 1
+done
+
+# set root password
+if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+  echo "Setting root password..."
+  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+fi
+
+# create database and user
+echo "Creating database and user..."
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+# create WordPress database
+echo "Creating WordPress database..."
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`wordpress\`;"
+
+# stop MariaDB server
+echo "Stopping MariaDB server..."
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+# start MariaDB server in foreground
+echo "Starting MariaDB server..."
+exec mysqld --user=mysql
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+set -x
+
+envsubst '${WORDPRESS_DB_NAME},${WORDPRESS_DB_USER},${WORDPRESS_DB_PASSWORD},${WORDPRESS_DB_HOST}' < /var/www/html/wordpress/wp-config.php.template > /var/www/html/wordpress/wp-config.php
+
 /usr/sbin/php-fpm8.3 -F
