ede72bc105a33594a53ec292fa2c428b229aa138
ilovenoah <ilovenoah0105@gmail.com>
2024-03-27 20:22:36 +0900

    tmp commit

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -x
 
-envsubst '${WORDPRESS_DB_NAME},${WORDPRESS_DB_USER},${WORDPRESS_DB_PASSWORD},${WORDPRESS_DB_HOST}' < /var/www/html/wordpress/wp-config.php.template > /var/www/html/wordpress/wp-config.php
+sed -i "s/WORDPRESS_DB_NAME/$WORDPRESS_DB_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/WORDPRESS_DB_USER/$WORDPRESS_DB_USER/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/WORDPRESS_DB_PASSWORD/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/WORDPRESS_DB_HOST/$WORDPRESS_DB_HOST/g" /var/www/html/wordpress/wp-config.php
 
 /usr/sbin/php-fpm8.3 -F
