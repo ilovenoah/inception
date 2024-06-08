029bcee9769e9e5ff05c8e734a54fc8da009a4e3
taiphyyy <ilovenoah0105@gmail.com>
2024-06-08 14:49:15 +0900

    fix small bug nginx conf

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i "s/DOMAIN_NAME/$DOMAIN_NAME" /etc/nginx/conf.d/default.conf
+set -x
+sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
