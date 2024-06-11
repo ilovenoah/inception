ccd0e485414e2dadfdac219d0ab2eb2dba04b591
ilovenoah <ilovenoah0105@gmail.com>
2024-06-11 18:08:26 +0900

    ssl

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -x
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
+sed -i "s#SSL_CERTIFICATE#/etc/nginx/ssl/rmatsuok.42.fr.crt#g" /etc/nginx/conf.d/default.conf
+sed -i "s#SSL_CERTIFICATE_KEY#/etc/nginx/ssl/rmatsuok.42.fr.key#g" /etc/nginx/conf.d/default.conf
+
 nginx -g "daemon off;"
