a7bdda49bbcb2be41f5968ec813dadc3214c71cc
ilovenoah <ilovenoah0105@gmail.com>
2024-06-11 18:35:24 +0900

    fiiiiiiiix

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -x
+
+if [ ! -f /etc/nginx/ssl/ssl_certificate.crt ]; then
+  mkdir -p /etc/nginx/ssl
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/ssl_certificate.key -out /etc/nginx/ssl/ssl_certificate.crt -subj "/CN=${DOMAIN_NAME}"
+fi
+
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
-sed -i "s#SSL_CERTIFICATE#/etc/nginx/ssl/ssl_certificate.crt#g" /etc/nginx/conf.d/default.conf
-sed -i "s#SSL_CERTIFICATE_KEY#/etc/nginx/ssl/ssl_certificate.key#g" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
