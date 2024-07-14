d03e7883f693948a0a829c88d430606bad825c49
ilovenoah <ilovenoah0105@gmail.com>
2024-07-14 18:31:18 +0900

    add secont wpuser

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -12,6 +12,7 @@ wp option update home "http://${DOMAIN_NAME}" --allow-root
 wp option update siteurl "http://${DOMAIN_NAME}" --allow-root
 if ! $(wp core is-installed --allow-root); then
   wp core install --url=${DOMAIN_NAME} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_DB_USER} --admin_password=${WORDPRESS_DB_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
+  wp user create ${WORDPRESS_SECOND_USER} ${WORDPRESS_SECOND_USER_EMAIL} --role=author --user_pass=${WORDPRESS_SECOND_USER_PASSWORD} --allow-root
 fi
 
 exec /usr/sbin/php-fpm8.3 -F
